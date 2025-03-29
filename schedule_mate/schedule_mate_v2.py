d7f85df6c225ad0a02dd961d0ca279ff2d5f7a9d
Admex666 <adam.jakus99@gmail.com>
2025-03-29 17:18:38 +0100

    minor updates

--- schedule_mate/schedule_mate_v2.py
+++ schedule_mate/schedule_mate_v2.py
@@ -21,17 +21,50 @@ def compare_day_end(day_end, date_to_check):
 
 # calculate free slots; df of start and end times
 def find_free_slots(tasks_fix_df, week_start, week_end, day_start, day_end):
-    free_slots = pd.DataFrame()
-    if not tasks_fix_df:
-        free_slots.loc[len(free_slots), ['start', 'end']] = [week_start, week_end]
-        return free_slots
+    free_slots = []
     
-    if tasks_fix_df.loc[0, 'start_date'] > week_start:
-        slot = [week_start, tasks_fix_df.loc[0, 'start_date']]
-        free_slots.loc[len(free_slots), ['start', 'end']] = slot
+    current_day = week_start.date()
+    while current_day <= week_end.date():
+        day_fix_tasks = tasks_fix_df[
+            (tasks_fix_df['start_date'].dt.date == current_day)
+            ].sort_values(by='start_date')
         
-    for i in range(1, len(tasks_fix_df)-1):
-        print(tasks_fix_df.loc[i,'start_date'])
+        day_start_dt = datetime.combine(current_day, day_start)
+        day_end_dt = datetime.combine(current_day, day_end)
+        
+        if day_fix_tasks.empty:
+            free_slots.append({'start': day_start_dt, 'end': day_end_dt})
+        else:
+            prev_end = day_start_dt
+
+            for _, row in day_fix_tasks.iterrows():
+                task_start = row['start_date']
+                task_end = row['end_date']
+
+                # Ha van szabad idő az előző esemény vége és a következő esemény kezdete között
+                if prev_end < task_start:
+                    free_slots.append({'start': prev_end, 'end': task_start})
+
+                prev_end = max(prev_end, task_end)  # Ne legyen átfedés
+
+            # Napi végéig fennmaradó szabad idő
+            if prev_end < day_end_dt:
+                free_slots.append({'start': prev_end, 'end': day_end_dt})
+
+        current_day += timedelta(days=1)
+
+    return pd.DataFrame(free_slots)
     
-    return free_slots
+free_slots = find_free_slots(tasks_fix_df, week_start, week_end, day_start, day_end)
+free_slots['duration'] = free_slots.end - free_slots.start
+free_slots.duration.sum()
+
+# Add flex tasks to free slots
+def redefine_free_slots(free_slots, task_start, task_end):
+
+def add_tasks_flex(tasks_flex_df, tasks_fix_df, free_slots):
     
+    schedule = pd.concat([tasks_fix, ]).sort_values(by='start')
+    return schedule
+
+add_tasks_flex(tasks_flex_df, tasks_fix_df, free_slots)
