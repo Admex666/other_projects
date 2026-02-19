12e0e69a759fc56424311b5d0aa4d013f048122c
admex666 <adam.jakus99@gmail.com>
2026-02-19 13:50:28 +0100

    vercel analytics added

--- storyturak_landing/src/app/layout.tsx
+++ storyturak_landing/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="hu" className={`${inter.variable} ${outfit.variable}`}>
       <body className="antialiased bg-background-dark text-foreground">
         {children}
+        <Analytics />
         <Script
           src="https://tally.so/widgets/embed.js"
           strategy="lazyOnload"
